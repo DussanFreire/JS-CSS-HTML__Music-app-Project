0005fd4e90e98e52b78f7404d580cc2bf646e70e
Dussan Freire <73646620+DussanFreire@users.noreply.github.com>
2021-06-12 17:53:01 -0400

    Se pueden agragar muchos roles a la vez

--- resources/js/createAcount.js
+++ resources/js/createAcount.js
@@ -1,52 +1,70 @@
 window.addEventListener("load", (event) => {
   const baseUrl = "http://localhost:16470/api/auth";
-  function login(event) {
-    debugger;
-    console.log(event.currentTarget);
-    event.preventDefault();
-    const userUrl = `${baseUrl}/User`;
-    const userRoleUrl = `${baseUrl}/UserRole`;
-    if (!Boolean(event.currentTarget.userName.value)) {
-      var usernameErrorElement = document.getElementById("login-errors");
-      usernameErrorElement.textContent = "username is requered";
-      usernameErrorElement.style.display = "block";
-      return;
-    }
+  async function login(event) {
+    try {
+      event.preventDefault();
+      const userUrl = `${baseUrl}/User`;
+      const userRoleUrl = `${baseUrl}/UserRole`;
+      if (!Boolean(event.currentTarget.userName.value)) {
+        var usernameErrorElement = document.getElementById("login-errors");
+        usernameErrorElement.textContent = "username is requered";
+        usernameErrorElement.style.display = "block";
+        return;
+      }
+      var createUserData = {
+        Email: event.currentTarget.userName.value,
+        Password: event.currentTarget.password.value,
+        ConfirmPassword: event.currentTarget.confirmPassword.value,
+      };
+      let adminRole = "";
+      let userRole = "";
+      if (event.currentTarget.roleAdmin.checked) {
+        adminRole = {
+          UserName: event.currentTarget.userName.value,
+          RoleName: "Admin",
+        };
+      }
+      if (event.currentTarget.roleUser.checked) {
+        userRole = {
+          UserName: event.currentTarget.userName.value,
+          RoleName: "User",
+        };
+      }
+      if (
+        event.currentTarget.roleUser.checked == false &&
+        event.currentTarget.roleAdmin.checked == false
+      ) {
+        alert("At least one role must be choose");
+        return;
+      }
 
-    var createUserData = {
-      Email: event.currentTarget.userName.value,
-      Password: event.currentTarget.password.value,
-      ConfirmPassword: event.currentTarget.confirmPassword.value,
-    };
-    var createUserRoleData = {
-      UserName: event.currentTarget.userName.value,
-      RoleName: event.currentTarget.roleName.value,
-    };
-
-    fetch(userUrl, {
-      headers: { "Content-Type": "application/json; charset=utf-8" },
-      method: "POST",
-      body: JSON.stringify(createUserData),
-    })
-      .then((response) => {
-        if (response.status !== 200) {
-          throw new Error(`Error (${response.status})`);
-        }
-        return fetch(userRoleUrl, {
+      let userReponse = await fetch(userUrl, {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: "POST",
+        body: JSON.stringify(createUserData),
+      });
+      debugger;
+      if (userReponse.status !== 200) {
+        throw new Error(`Error (${response.status})`);
+      }
+      if (adminRole !== "") {
+        let fetchedAdminRoles = await fetch(userRoleUrl, {
           headers: { "Content-Type": "application/json; charset=utf-8" },
           method: "POST",
-          body: JSON.stringify(createUserRoleData),
+          body: JSON.stringify(adminRole),
         });
-      })
-      .then((response) => {
-        if (response.status === 200) {
-          alert("Account created!");
-          window.location.href = "login.html";
-        }
-      })
-      .catch((response) => {
-        console.log(response);
-      });
+      }
+
+      if (userRole !== "") {
+        let fetchedAdminRoles = await fetch(userRoleUrl, {
+          headers: { "Content-Type": "application/json; charset=utf-8" },
+          method: "POST",
+          body: JSON.stringify(userRole),
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function goToCreateAccount() {
@@ -59,3 +77,12 @@ window.addEventListener("load", (event) => {
     .getElementById("go-back")
     .addEventListener("click", goToCreateAccount);
 });
+// ;
+//         let lastResponse = response[1];
+//         debugger;
+
+//         if (lastResponse.status === 200) {
+//           alert("Account created!");
+//           window.location.href = "login.html";
+//         }
+//       })
